45675ee5467857e87e0d19c4df2196420b3ab176
rrosztoczy <rrosztoczy@gmail.com>
2019-04-16 13:00:24 -0400

    done

--- jsfiles/monsters.js
+++ jsfiles/monsters.js
@@ -24,27 +24,18 @@ var MonsterIcon = L.Icon.extend({
     // Create monster icon(marker) and add to the map
     function renderMonster(monster) {
       // randomly generate monster location
+      const monsterLatitude = parseFloat((Math.random() * (51.51000 - 51.50245) + 51.50245).toFixed(7));
+      const monsterLongitude = parseFloat((Math.random() * (-0.15621 + 0.18758) - 0.18758).toFixed(7));
+      const monsterLocation = [monsterLatitude, monsterLongitude];
+      const monsterBoundary = new L.Circle(monsterLocation, 75, {color: 'red', opacity: 0.001}).addTo(map)
+      monsterBoundaries.push(monsterBoundary)
+      monster.monsterBorder = monsterBoundary
       console.log(monster.image)
       const monsterIcon = new MonsterIcon({iconUrl: monster.image});
       console.log(monsterIcon)
-      const newMonsterMarker = L.marker(generateMonsterLocation(), {icon: monsterIcon}).addTo(map).bindPopup(monster.name + ": " + monster.phrase)
+      const newMonsterMarker = L.marker(monsterLocation, {icon: monsterIcon}).addTo(map).bindPopup(monster.name + ": " + monster.phrase)
       
-      // randomly generate interaction border use .002 as starting distance attempt? How do I do this.... need to persist
-      // an interaction border that interferes with movement and triggers the event
-  
-      // Associate marker with monster
       monster.marker = newMonsterMarker
   
-      // Add icon from DB
     };
 
-  
-    function generateMonsterLocation() {
-    // generate random gps within bounds *need numbers rather than variables for fixed*
-    const monsterLatitude = parseFloat((Math.random() * (51.51000 - 51.50245) + 51.50245).toFixed(7));
-    const monsterLongitude = parseFloat((Math.random() * (-0.15621 + 0.18758) - 0.18758).toFixed(7));
-    const monsterLocation = [monsterLatitude, monsterLongitude];
-    const monsterBoundary = new L.Circle(monsterLocation, 75, {color: 'red', opacity: 0.001}).addTo(map)
-    monsterBoundaries.push(monsterBoundary)
-    return monsterLocation
-    }
