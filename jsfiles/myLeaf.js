d33ae2706ba2d4ed4e49579d92a52c525ae753d4
rrosztoczy <rrosztoczy@gmail.com>
2019-04-17 17:13:12 -0400

    done

--- jsfiles/myLeaf.js
+++ jsfiles/myLeaf.js
@@ -8,6 +8,7 @@ let monsterAdapter = adapter('http://localhost:3000/api/v1/monsters')
 let monstersFromAPI = [];
 let wildMonsters = [];
 let caughtMonsters = [];
+let selectedMonster = {}
 
 let caughtCode = [];
 let monsterBoundaries = [];
@@ -64,47 +65,132 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+//********************************Game Application Run*************************************/
+
+
 document.addEventListener("keydown", movePlayer);
-// Listen for and execute player movement
+
+
+//********************************Player Movement Logic*************************************/
+
+// Execute movement
 function movePlayer(e) {
-    // Verify movement is within map boundary
-    // Basically, once I hit this if conditional Im always there. So need to somehow prevent form hitting
-      console.log("right border", rightBorder)
-      console.log("right buffer",rightBorder - borderBuffer)
-      console.log("current lat", currentLatitude)
-      console.log("current long", currentLongitude)
-      // Player step logic
-      switch (e.keyCode) {
-      case 39: playerStep(right)
-      break;
-      case 37: playerStep(left)
-      break;
-      case 38: playerStep(up)
-      break;
-      case 40: playerStep(down)
-      break;
-      default: alert("Please press an arrow key");
-    }
-  // myIcon.bindPopup(warning).openPopup();
-  // setTimeout( () => {
-  //   map.closePopup()
-  // }, 2000)}
+  switch (e.keyCode) {
+  case 39: playerStep(right)
+  break;
+  case 37: playerStep(left)
+  break;
+  case 38: playerStep(up)
+  break;
+  case 40: playerStep(down)
+  break;
+  default: alert("Please press an arrow key");
 }
+}
+
+// Step direction movement and boundary checks
+function left() {
+  if (leftBorder <= currentLongitude + borderBuffer) {
+    horizontal -= 0.0005;
+  }
+}
+function right() {
+  if (rightBorder >= currentLongitude - borderBuffer) {
+    horizontal += 0.0005;
+  }
+}
+function up() {
+  if (topBorder >= currentLatitude + borderBuffer) {
+    vertical += 0.0005;
+  }
+}
+function down() {
+  if (bottomBorder <= currentLatitude - borderBuffer) {
+  vertical -= 0.0005;
+}
+}
+
+function playerStep(direction) {
+  map.removeLayer(icon);
+  map.removeLayer(circle);
+  let newIcon;
+  let newCircle;
+  direction()
+  center = [latitude + vertical, longitude + horizontal];
+  currentLatitude = center[0]
+  currentLongitude = center[1]
+  newIcon = L.marker(center, {
+    autoPan: true,
+    autoPanSpeed: 10,
+    icon: myIcon,
+    zIndexOffset: 1000
+  }).addTo(map)
+  icon = newIcon;
+  newCircle = new L.Circle(center, 35, {color: 'red', opacity: 0.001}).addTo(map)
+  circle = newCircle;
+  map.panTo(icon.getLatLng());
+
+  // check for Codemon interaction
+  initiateQuiz(checkIntersection())
+}
+
+// ******************************************Codemon Interaction Logic******************************
 
 function checkIntersection() {
   let foundMonster = wildMonsters.find(monster => {
     return monster.monsterBorder.getBounds().intersects(circle.getBounds())
-  }
-  )  
-  return foundMonster
+  })  
+    return foundMonster
 }
 
-  function initiateQuiz(monster) {
-    if (monster) {
-        // TODO: initiatePopup(monster)
-        renderBattle(monster)
-    }
+function initiateQuiz(monster) {
+  if (monster) {
+      // TODO: initiatePopup(monster)
+      renderBattle(monster)
   }
+}
+
+quizContainer.addEventListener('click', function(e){
+  if (e.target.dataset.correct === "true") {
+    catchMonster(setMonster(e))
+  } else if (e.target.dataset.correct === "false") {
+    monsterFled(setMonster(e))
+  }
+})
+
+// *****************************Code for Quiz***********************************************
+
+function renderQuestion(monster) {
+  const questionBox = document.createElement('div')
+  // TODO: refactor to pull random question from this monsters question list after mvp
+  questionBox.className = "question"
+  questionBox.innerText = monster.questions[0].question_text
+  quizContainer.appendChild(questionBox)
+  questionBox.style.gridArea = "quizHeader"
+}
+
+function renderAnswer(answer) {
+  const answerBox = document.createElement('div')
+  answerBox.className = "answer"
+  answerBox.dataset.letter = answer.letter
+  answerBox.dataset.correct = answer.correct
+  answerBox.innerText = answer.letter + ". " + answer.answer_text
+  quizContainer.appendChild(answerBox)
+  answerBox.style.gridArea = "question"+answer.letter
+}
+
+function renderBattle(monster) {
+  // extend this to render the battle scene, pause other activity, render question, etc.
+  quizContainer.dataset.monstername = monster.name
+  renderQuestion(monster)
+  monster.answers.forEach(renderAnswer)
+}
+
+// ********************************Interaction Outcome Logic*******************************
+function setMonster(e) {
+  selectedMonster = wildMonsters.find(monster => monster.name === e.target.parentNode.dataset.monstername)
+  return selectedMonster
+} 
 
 function clearQuiz() {
   quizContainer.innerHTML = ""
@@ -130,60 +216,8 @@ function catchMonster(monster) {
   clearQuiz()
 }
 
+// **********************************Successful Catch Functions**************************************
 
-  // *********************************Player Step Functionality*******************
-function left() {
-  if (leftBorder <= currentLongitude + borderBuffer) {
-    horizontal -= 0.0005;
-  }
-}
-function right() {
-  if (rightBorder >= currentLongitude - borderBuffer) {
-    horizontal += 0.0005;
-  }
-}
-function up() {
-  if (topBorder >= currentLatitude + borderBuffer) {
-    vertical += 0.0005;
-  }
-}
-function down() {
-  if (bottomBorder <= currentLatitude - borderBuffer) {
-  vertical -= 0.0005;
-}
-}
-  //right arrow logic
-
-function playerStep(direction) {
-  map.removeLayer(icon);
-  map.removeLayer(circle);
-  let newIcon;
-  let newCircle;
-  direction()
-  center = [latitude + vertical, longitude + horizontal];
-  currentLatitude = center[0]
-  currentLongitude = center[1]
-  newIcon = L.marker(center, {
-    autoPan: true,
-    autoPanSpeed: 10,
-    icon: myIcon,
-    zIndexOffset: 1000
-  }).addTo(map)
-  icon = newIcon;
-  newCircle = new L.Circle(center, 35, {color: 'red', opacity: 0.001}).addTo(map)
-  circle = newCircle;
-  map.panTo(icon.getLatLng());
-  initiateQuiz(checkIntersection())
-}
-
-
-//function to help us find longitude and latitude on our map
-// function onMapClick(e) {
-//   alert("You clicked the map at " + e.latlng);
-// }
-// map.on('click', onMapClick);
-
-// *****************Code for Quiz***************************** Refactor out interaction logic, quiz logic etc.
 function renderCodemonBelt(monster) {
   renderNumber(monster)
   renderCaughtCodemonImg(monster)
@@ -216,40 +250,12 @@ function renderCaughtCodemonName(monster) {
   caughtName.style.gridArea = "codemon" + monster.caughtOrder + "Name"
 }
 
-quizContainer.addEventListener('click', function(e){
-  if (e.target.dataset.correct === "true") {
-    const selectedMonster = wildMonsters.find(monster => monster.name === e.target.parentNode.dataset.monstername)
-    catchMonster(selectedMonster)
-  } else {
-    const selectedMonster = wildMonsters.find(monster => monster.name === e.target.parentNode.dataset.monstername)
-    console.log(`${selectedMonster.name} ran away!`)
-    monsterFled(selectedMonster)
-  }
-})
 
-function renderBattle(monster) {
-  // extend this to render the battle scene, pause other activity, render question, etc.
-  quizContainer.dataset.monstername = monster.name
-  renderQuestion(monster)
-  monster.answers.forEach(renderAnswer)
-}
+//---------------------------helper -------------------
+// function to help us find longitude and latitude on our map
+// function onMapClick(e) {
+//   alert("You clicked the map at " + e.latlng);
+// }
+// map.on('click', onMapClick);
 
-function renderQuestion(monster) {
-  const questionBox = document.createElement('div')
-  // TODO: refactor to pull random question from this monsters question list after mvp
-  questionBox.className = "question"
-  questionBox.innerText = monster.questions[0].question_text
-  quizContainer.appendChild(questionBox)
-  questionBox.style.gridArea = "quizHeader"
-}
-
-function renderAnswer(answer) {
-  const answerBox = document.createElement('div')
-  answerBox.className = "answer"
-  answerBox.dataset.letter = answer.letter
-  answerBox.dataset.correct = answer.correct
-  answerBox.innerText = answer.letter + ". " + answer.answer_text
-  quizContainer.appendChild(answerBox)
-  answerBox.style.gridArea = "question"+answer.letter
-}
 
